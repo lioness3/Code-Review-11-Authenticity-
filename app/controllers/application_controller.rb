b32e1a0ffb814d26e428aee3e0945470d1adb615
lioness3 <jcnailtech3@yahoo.com>
2020-01-24 11:01:32 -0800

    migrate user into db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def authorize
+    if !current_user
+      flash[:alert] = "You aren't authorized to visit that page."
+      redirect_to '/'
+    end
+  end 
 end
--- db/migrate/20200124183811_create_users.rb
+++ db/migrate/20200124183811_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :email
       t.string :username
+      t.boolean :admin, default: false
       t.string :password_hash
       t.string :password_salt
     end
